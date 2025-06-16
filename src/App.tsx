1aa86a68e47e9ad4230a3f1596e46b20d7b4d1c5
Lucas <lxcasgarcia09@gmail.com>
2025-06-16 14:59:02 -0300

    Verificando acertos do usuário e deixando a dica automático

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,11 @@ import type { LettersUsedProps } from "./components/LettersUsed"
 
 export default function App() {
 
+  const [score, setScore] = useState(0)
   const [attempts, setAttempts] = useState(0)
   const [letter, setLetter] = useState("")
-    const [lettersUsed, setLettersUsed] = useState<LettersUsedProps[]>([])
-  const [challenge, setChallenge ] = useState<Challenge | null>(null)
+  const [lettersUsed, setLettersUsed] = useState<LettersUsedProps[]>([])
+  const [challenge, setChallenge] = useState<Challenge | null>(null)
 
 
   function handleRestartGame() {
@@ -33,23 +34,29 @@ export default function App() {
     setLetter("")
   }
 
-  function handleConfirm(){
-    if(!challenge) {
+  function handleConfirm() {
+    if (!challenge) {
       return
     }
 
-    if(!letter.trim()) {
-      return alert ("Digite uma letra!")
+    if (!letter.trim()) {
+      return alert("Digite uma letra!")
     }
 
     const value = letter.toUpperCase()
     const exist = lettersUsed.find((used) => used.value.toUpperCase() === value)
 
-    if(exist) {
+    if (exist) {
       return alert("Você já utilizou a letra " + value)
     }
 
-    setLettersUsed((prevState) => [...prevState, { value, correct: false}])
+    const hits = challenge.word.toUpperCase().split("").filter((char) => char === value).length
+
+    const correct = hits > 0
+    const currentScore = score + hits
+
+    setLettersUsed((prevState) => [...prevState, { value, correct }])
+    setScore(currentScore)
 
     setLetter("")
   }
@@ -58,33 +65,33 @@ export default function App() {
     startGame()
   }, [])
 
-  if(!challenge) {
+  if (!challenge) {
     return
   }
 
   return (
     <div className={styles.container}>
-        <main>
-          <Header current={attempts} max={10} onRestart={handleRestartGame}/>
+      <main>
+        <Header current={attempts} max={10} onRestart={handleRestartGame} />
 
-          <Tip tip="Linguagem de programação dinâmica" />
+        <Tip tip={challenge.tip} />
 
-          <div className={styles.word}>
-            {challenge.word.split("").map(() => (
-                <Letter value="" />
-              ))}
-          </div>
-          
-          <h4>Palpite</h4>
+        <div className={styles.word}>
+          {challenge.word.split("").map(() => (
+            <Letter value="" />
+          ))}
+        </div>
 
-          <div className={styles.guess}>
-            <Input autoFocus maxLength={1} placeholder="?" value={letter} onChange={(e) => setLetter(e.target.value)}/>
-            <Button title="Confirmar" onClick={handleConfirm}/>
-          </div>
+        <h4>Palpite</h4>
 
-          <LettersUsed data={lettersUsed}/>
-          
-        </main>
+        <div className={styles.guess}>
+          <Input autoFocus maxLength={1} placeholder="?" value={letter} onChange={(e) => setLetter(e.target.value)} />
+          <Button title="Confirmar" onClick={handleConfirm} />
+        </div>
+
+        <LettersUsed data={lettersUsed} />
+
+      </main>
     </div>
   )
 }

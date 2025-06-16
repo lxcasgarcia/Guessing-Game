fa4d8519db33002853237da1d79f0729909cee31
Lucas <lxcasgarcia09@gmail.com>
2025-06-16 14:47:49 -0300

    Criando o handleConfirm

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,27 @@ export default function App() {
     setLetter("")
   }
 
+  function handleConfirm(){
+    if(!challenge) {
+      return
+    }
+
+    if(!letter.trim()) {
+      return alert ("Digite uma letra!")
+    }
+
+    const value = letter.toUpperCase()
+    const exist = lettersUsed.find((used) => used.value.toUpperCase() === value)
+
+    if(exist) {
+      return alert("Você já utilizou a letra " + value)
+    }
+
+    setLettersUsed((prevState) => [...prevState, { value, correct: false}])
+
+    setLetter("")
+  }
+
   useEffect(() => {
     startGame()
   }, [])
@@ -57,8 +78,8 @@ export default function App() {
           <h4>Palpite</h4>
 
           <div className={styles.guess}>
-            <Input autoFocus maxLength={1} placeholder="?"/>
-            <Button title="Confirmar" />
+            <Input autoFocus maxLength={1} placeholder="?" value={letter} onChange={(e) => setLetter(e.target.value)}/>
+            <Button title="Confirmar" onClick={handleConfirm}/>
           </div>
 
           <LettersUsed data={lettersUsed}/>

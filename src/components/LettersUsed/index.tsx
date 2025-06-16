688b33cf5760e4e4c19e84b0dd72a6ba64a7424e
Lucas <lxcasgarcia09@gmail.com>
2025-06-16 09:28:20 -0300

    Criando a variação de tamanhos das letters

--- src/components/LettersUsed/index.tsx
+++ src/components/LettersUsed/index.tsx
@@ -1,9 +1,16 @@
 import styles from "./styles.module.css"
 
+import { Letter } from "../Letter"
+
 export function LettersUsed(){
     return (
         <div className={styles.lettersUsed}>
             <h5>Letras utilizadas</h5>
+
+            <div>
+                <Letter value="A" size="small"/>
+                <Letter value="B" size="small"/>
+            </div>
         </div>
     )
 }
